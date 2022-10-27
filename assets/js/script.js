9cbf6c15b1a58a87171eef325ee21ad5d2140966
luisa <b.luuisa77@gmail.com>
2022-10-26 17:58:16 -0700

    all changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,21 +7,31 @@ document.addEventListener('DOMContentLoaded', function () {
     console.log('Hi')
 
     // TODO: Add event listener and handler for flip and clear buttons
-
+flipBtn.addeventlistener("click", function () {
     // Flip Button Click Handler
-        // TODO: Determine flip outcome
-        // TODO: Update image and status message in the DOM
-
-        // Update the scorboard
-        // TODO: Calculate the total number of rolls/flips
-        // Make variables to track the percentages of heads and tails
-        // TODO: Use the calculated total to calculate the percentages
-        // HINT: Make sure not to divide by 0! (if total is 0, percent will be 0 as well)
-        // TODO: Update the display of each table cell
+    
+    // TODO: Determine flip outcome
+    let random =Math.random();
+        console.log(fkip);
+        if (flip < 0.5) {
+            image.src = tailsImg;
+            statusMsgDiv.textContent = "You got Tails";
+            console.l0g("tails");
+        } else if (flip >= 0.5) {
+            image.src=headsImg;
+            statusMsgDiv.textContent = "You got Heads";
+        }
+    // TODO: Update image and status message in the DOM
+    // Update the scorboard
+    // TODO: Calculate the total number of rolls/flips
+    // Make variables to track the percentages of heads and tails
+    // TODO: Use the calculated total to calculate the percentages
+    // HINT: Make sure not to divide by 0! (if total is 0, percent will be 0 as well)
+    // TODO: Update the display of each table cell
 
 
     // Clear Button Click Handler
-        // TODO: Reset global variables to 0
-        // TODO: Update the scoreboard (same logic as in flip button click handler)
+    // TODO: Reset global variables to 0
+    // TODO: Update the scoreboard (same logic as in flip button click handler)
 
 })
